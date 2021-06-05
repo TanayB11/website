311c77df9f0c76913c2e42c4f0eff9045ff8292e
TanayB11 <tanaybiradar24@gmail.com>
2021-06-04 22:27:47 -0700

    Add meta information

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,14 +7,14 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'web',
+    title: 'Quantum Computing Initiative',
     htmlAttrs: {
       lang: 'en'
     },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' }
+      { hid: 'description', name: 'description', content: 'A 3-week introductory course in quantum computing for high school students' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
